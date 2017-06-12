5c87ecad3e42f8d6adc04fb8818825ca0e194e6a
György István <igysuli@gmail.com>
2017-06-12 08:13:04 +0200

    Refactoring: prototyping smarter + removing space before parameter list + fixing copy paste argument smell

--- model/warrior.js
+++ model/warrior.js
@@ -5,16 +5,19 @@ function Warrior(hp)
     this._hp = parseInt(hp) > 30 ? 30 : parseInt(hp);
 }
 
-Warrior.prototype.getHP = function () {
-    return this._hp;
-};
-
-Warrior.prototype.attack = function (anotherWarrior) {
-    anotherWarrior.decreaseHPByOne();
-};
-
-Warrior.prototype.decreaseHPByOne = function (anotherWarrior) {
-    this._hp--;
+Warrior.prototype = {
+    
+    getHP: function() {
+        return this._hp;
+    },
+    
+    attack: function(anotherWarrior) {
+        anotherWarrior.decreaseHPByOne();
+    },
+    
+    decreaseHPByOne: function() {
+        this._hp--;
+    }
 };
 
 module.exports = Warrior;
